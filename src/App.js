67bd09f44da12c805defb6b6bced85d7d18b4b73
ponkesito22 <71133754+DanielaAlvarado22@users.noreply.github.com>
2023-02-15 18:12:13 -0600

    first
    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 import './App.css';
 
+
+
 function App() {
+  
+  const [ poke, setPoke ] = useState([]);  
+  const getPoke = async(pokeName) => {
+    try{
+      let res = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`)
+      let data = res.data;
+      setPoke(data)
+    }catch(error) {console.error(error)}
+  }
+
+  useEffect(  () => {
+    const value = prompt('Escribe el id o nombre del pokemon');
+    
+   getPoke(value)
+  },[]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+        {JSON.stringify(poke, null, 2)} 
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+       
+      
     </div>
   );
 }
